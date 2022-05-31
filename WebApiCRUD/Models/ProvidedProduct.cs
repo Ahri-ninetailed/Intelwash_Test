e4dd607cc84b714c0b7e0c99c5efe091217fdc9b
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2022-05-31 20:12:15 +0300

    Добавил таблицу ProvidedProducts в базу данных

---
+++ WebApiCRUD/Models/ProvidedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace WebApiCRUD.Models
+{
+    public class ProvidedProduct
+    {
+       
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int ProductQuantity
+        {
+            get => productQuantitty;
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Количество товара не может быть меньше нуля.");
+                productQuantitty = value;
+            }
+        }
+        private int productQuantitty;
+    }
+}
